65900d9caa68249e761944ba2269a414bb6ab5bc
wslaurance <wslaurance@missionfocus.com>
2013-08-05 16:45:06 -0400

    add a mocha test

---
+++ test.js
@@ -0,0 +1,8 @@
+var em = require('./ember'),
+    assert = require('assert');
+
+describe('exported ember object', function(){
+  it('should be equal to something', function() {
+    assert.ok(em);
+  });
+});
